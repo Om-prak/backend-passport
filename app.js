23b89a660381ca076560d354c0f7ce0880c57123
prakash <prakash.v124421@gmail.com>
2024-03-12 11:05:59 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -51,8 +49,6 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -60,20 +56,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(4000,()=>{
+  console.log('server running with port 4000');
 });
 
 module.exports = app;
